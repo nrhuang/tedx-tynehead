f70a616a6a93ef90f76fc0d9d370aa43ee94513d
nrhuang <63937454+nrhuang@users.noreply.github.com>
2021-05-14 20:18:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 //mostly just animations and maybe a form if we can figure out how to make that, otherwise we'll use google forms or smth
 //what kind of animations?
+//dropdowns and maybe something that happens when the user scrolls down
