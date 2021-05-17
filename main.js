d94b05ec55cf0d93303ffa484281127f13cb31d8
samtwwong <samxdw.23@gmail.com>
2021-05-16 21:06:55 -0700

    slideshow time bois

--- main.js
+++ main.js
@@ -2,82 +2,31 @@
 //what kind of animations?
 //dropdowns and maybe something that happens when the user scrolls down
 
-// do not mind what i did down there there might be a cat on the website
-var secret = "3838404037393739666513"; //Konami Codeeeeee
-var input = "";
-var timer;
-var mode = false;
-var showkonami =false;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$(document).ready(function() {
-	$('#flash').hide();
-});
-
-$(document).keyup(function(e) {
-	//alert(e.which);
-	input += e.which;
-
-	clearTimeout(timer);
-	timer = setTimeout(function() { input = ""; }, 500);
-	check_input();
-});
-
-function check_input() {
-	if(input == secret) {
-	//the secret code
-	showkonami= true;
-}};
-
-$(document).ready(function() {
-	setInterval(function() { $('#info').html('Keystroke: ' + input); }, 100);
-});
-
-$(function() {
-var konami = $('.konami');
-
-konami.css("left", walking+"%");
-
-var horizontal = konami.css('background-position-x');
-
-var flag = 0; var scroll = 23;
-console.log(walking);
-
-$(document).on("scroll", function() {
-
-if(showkonami == true){
-
-var scroll = $(document).scrollTop();
-walking = walking - 1;
-konami.css("left", walking+"%");
-
-if (walking == -10){
-walking = 110; }
-
-if ((scroll % 3) == 0){
-
-if(horizontal == "0px"){
-konami.css("background-position-x", "-65px");
-
-horizontal=konami.css('background-position-x');
-
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-else if ((horizontal == "-65px") && (flag == 0)){
-konami.css("background-position-x", "-130px");
 
-horizontal=konami.css('background-position-x');
-
-flag = 1;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-else if ((horizontal == "-65px") && (flag == 1)){
-konami.css("background-position-x", "0px");
 
-horizontal=konami.css('background-position-x');
-
-flag = 0;
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  	if (n < 1) {slideIndex = slides.length}
+  	for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-else if(horizontal == "-130px"){
-konami.css("background-position-x", "-65px");
-
-horizontal=konami.css('background-position-x');
-
-} } } }); });
